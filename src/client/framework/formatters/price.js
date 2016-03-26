87708ba0dcfa32325c5283e49fe4b96b440f3157
Boris Karbanovich <bacsus_beacks@mail.ru>
2016-03-26 16:56:48 +0300

    [Cleanup]: removed redundant code & img.

--- src/client/framework/formatters/price.js
+++
@@ -1,7 +0,0 @@
-let intlNumberFormat = new Intl.NumberFormat('ru', {
-	style: 'currency',
-	currency: 'RUB',
-	maximumSignificantDigits: 3
-});
-
-export default intlNumberFormat.format.bind(intlNumberFormat);
--- src/server/app.js
+++ src/server/app.js
@@ -2,21 +2,4 @@ import https from 'https';
 import config from './config';
 import app from './webpack-server';
 
-app.use('/cpu', (req, res) => {
-	let options = {
-		host: 'catalog.api.onliner.by',
-		path: '/facets/cpu',
-		method: 'GET',
-		headers: { 'Accept': 'application/json' }
-	};
-	let xhr = https.request(options, xhrRes => {
-		let page = '';
-		//xhrRes.setEncoding('utf8');
-		xhrRes.on('data', chunk => page += chunk);
-		xhrRes.on('end', () => res.write(page));
-	});
-	xhr.write('');
-	xhr.end();
-});
-
 app.listen(config.PORT, config.HOST);
